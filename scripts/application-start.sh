0288c713a4c27ac2b78dcdd95dc4f8b55ac5282e
Jedidiah McKean <jedidiahmckean1990@gmail.com>
2020-07-31 15:53:07 -0400

    codepipeline test:2

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /var/www/html/ims-app/api;
-pm2 restart index;
-pm2 startup;
+sudo pm2 start index.js;
+sudo pm2 startup;
 sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ec2-user --hp /
