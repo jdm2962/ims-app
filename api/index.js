831accd941e4b594a764c0745b7760c547d43968
Jedidiah McKean <jedidiahmckean1990@gmail.com>
2020-08-04 07:07:09 -0400

    Updated GET /api/item/:category/:name route. Added POST /api/item(insert an item) route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const uuid = require('uuid')
 
 const aws = require('aws-sdk');
 const dynamo = new aws.DynamoDB({'region' : 'us-east-1'});
@@ -12,7 +13,7 @@ const port = 3000;
 
 const tableName = 'ims-table';
 
-let resData = [];
+let resData;
 
 
 // home route
@@ -24,17 +25,20 @@ app.get('/api', (req, res) => {
 app.get('/api/items', (req, res) => {
 	const params = {
 	TableName : tableName,
-	// ProjectionExpression : 'category'
 	};
+	resData = [];
 
 	dynamo.scan(params, (err, data) => {
 		if(err) throw err;
 		data['Items'].map((item) => {
 			resData.push(
 				{
-					category : item.category.S,
-					id : item.itemId.N,
-					name : item.name.S
+					'category' : item.category.S,
+					'id' : item.id.S,
+					'name' : item.name.S,
+					'quantity' : item.quantity.N,
+					'packages' : item.packages.N,
+					'quantityPerPackage' : item.quantityPerPackage.N
 				}
 			);
 		});
@@ -45,44 +49,93 @@ app.get('/api/items', (req, res) => {
 });
 
 
-// get a single item category and item name
+// get item by id
+app.get('/api/item/:id', (req, res) => {
+
+});
+
+
+// get a single item by category and item name
 app.get('/api/item/:category/:name', (req, res) => {
-	const id = req.params.id;
 	const category = req.params.category;
 	const name = req.params.name;
 	const params = {
 		TableName : tableName,
-		IndexName : "category-name-index",
-		ExpressionAttributeNames : {
-			'#n' : 'name'
-		},
-		ExpressionAttributeValues : {
-			':name' : {"S" : name},
-			':category' : {"S" : category}
-		},
-		KeyConditionExpression : `category = :category AND #n = :name`
+		Key : {
+			'category' : {'S' : category},
+			'name' : {'S' : name}
+		}
 	};
 
-
-	dynamo.query(params, (err, data) => {
+	dynamo.getItem(params, (err, data) => {
 		if(err){
 			console.log(err);
 			res.status(500).send('Server Error');
 		}
-		if(data.Count === 0) res.status(404).send('Sorry, it looks like that item doesn\'t exist');
+
+		else if(data.Item === undefined || data.Item === null) {
+			res.status(404).send('Sorry, it looks like that item doesn\'t exist');
+		}
 		else {
 			resData = {
-				'category' : data.Items[0].category.S,
-				'name' : data.Items[0].name.S,
-				'id' : data.Items[0].itemId.N
+				'category' : data.Item.category.S,
+				'name' : data.Item.name.S,
+				'id' : data.Item.id.S,
+				'quantity' : data.Item.quantity.N,
+				'packages' : data.Item.packages.N,
+				'quantityPerPackage' : data.Item.quantityPerPackage.N
 			}
-			res.json(resData);	
+			res.json(resData);
+			resData = [];
 		}
 	});
+
+
+	
 
 });
 
 // insert an item w/ attributes
+
+app.post('/api/item', (req, res) => {
+	let id = uuid.v4();
+	let name = (!req.query.name) ? '' : req.query.name.toLowerCase();
+	let category = (!req.query.category) ? 'uncategorized' : req.query.category.toLowerCase();
+	let quantity = (!req.query.quantity) ? 0 : req.query.quantity;
+	let packages = (!req.query.packages) ? 0 : req.query.packages;
+	let quantityPerPackage = (!req.query.quantityPerPackage) ? 0 : req.query.quantityPerPackage;
+
+	const params = {
+		TableName : tableName,
+		Item : {
+			'id' : {'S' : id},
+			'category' : {'S' : category},
+			'name' : {'S' : name},
+			'quantity' : {'N' : quantity.toString()},
+			'packages' : {'N' : packages.toString()},
+			'quantityPerPackage' : {'N' : quantityPerPackage.toString()}
+
+		},
+		ConditionExpression : 'attribute_not_exists(id)', 
+	};
+	// check to see if item attributes have been given
+	if(Object.keys(req.query).length === 0) res.status(400).send("Error, need to enter item attributes.");
+	else if(!name) res.status(400).send("Error, must enter an item name.");
+	else
+	{
+		dynamo.putItem(params, (err, data) => {
+			if(err){
+				if(err.code === 'ConditionalCheckFailedException') res.status(400).send("Item already exists.");
+				else{
+					console.log(err);
+					res.status(500).send('Server Error');
+				}
+			}
+			else res.status(200).send("Success. Item entered");
+		});
+	}
+
+});
 
 // delete an item by id
 
