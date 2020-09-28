95cbe2572ca7d1e7475dea7292fdaa95132d2d8d
Jedidiah McKean <jedidiahmckean1990@gmail.com>
2020-09-28 15:20:46 -0400

    Added React

--- ims-react/src/index.js
+++ ims-react/src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-import 'bulma/css/bulma.css';
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-import Header from './components/header.js';
-import Body from './components/body.js';
-import Footer from './components/footer.js';
-
-
-
-
-class App extends React.Component{
-
-  render(){
-    return(
-      <div className = "">
-        <Header />
-        <Body />
-        <Footer />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
